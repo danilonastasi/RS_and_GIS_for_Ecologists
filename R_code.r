fc5021c52521448607586a317865fce290e2e876
danilonastasi <100956845+danilonastasi@users.noreply.github.com>
2023-12-07 21:25:11 +0300

    Update R_code.r

--- R_code.r
+++ R_code.r
@@ -45,18 +45,24 @@ studyarea <- st_read("c:...filepath")
 ########### book: RS and GIS for Ecologists - paragraph 3.2.1 ################
 ###########                                                   ################
 
-# exporting, from USGS, landsat results as a comma separated value file, zip file, txt file, saving in our drive:
+# exporting, from USGS, landsat results 2001-2011 as a comma separated value file, zip file, txt file, saving in our drive:
 
 zipfiles <- list.files(path = "C:/Users/danil/Downloads/landsat/usgsexport", pattern = ".zip", full.names = TRUE)
 # unzip:
 eeResults <- lapply(zipfiles, unzip, exdir = "C:/Users/danil/Downloads/landsat/usgsexport")
 
-# EEdatatable <- read.table("C:/Users/danil/Downloads/landsat/usgsexport/landsat_tm_c2_l1_6571f99e3ee92d9c.txt", sep=",", header=FALSE)
+# eedatatable <- read.table("C:/Users/danil/Downloads/landsat/usgsexport/landsat_tm_c2_l1_6571f99e3ee92d9c.txt", sep=",", header=FALSE)
 # read.table looks similar result with read.csv
 
-EEdatacsv <- read.csv("C:/Users/danil/Downloads/landsat/usgsexport/landsat_tm_c2_l1_6571f99e3ee92d9c.txt", sep=",", header=FALSE)
-head(EEdatacsv,10)  # let's show the first 10 rows
+eedatacsv <- read.csv("C:/Users/danil/Downloads/landsat/usgsexport/landsat_tm_c2_l1_6571f99e3ee92d9c.txt", sep=",", header=FALSE)
+head(eedatacsv,10)  # let's show the first 10 rows
 
 
+# temp, to test #
+ggplot(subset(eedatacsv, path = 224 & row = 63 & Cloud Cover < 20)) +
+   geom_tile(aes(x = Doy, alpha = Cloud Cover,
+                 fill = Satellite), width = 2, size = 2) +
+   scale_y_continuous(breaks = c(2001:2011)) +
+   scale_alpha_continuous(name = "Cloud Cover (%)", range = c(1, 0.5))
 
 
